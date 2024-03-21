0c5ce4b68451e55933daa37befa7f922d1c822d2
LMadrigalArellano@Deloitte.com <LMadrigalArellano@Deloitte.com>
2024-03-21 11:21:47 -0600

    LDMA:
    Navbar items are now dynamic

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,6 +1,12 @@
 import { HomeIcon } from "@primer/octicons-react"
 import Link from "next/link"
 
+const navItems = [
+	{ path: '/catalog', text: 'Catalog' },
+	{ path: '/', text: 'Dynamic 1' },
+	{ path: '/', text: 'Dynamic 2' },
+];
+
 export const Navbar = () => {
 	return (
 		<nav className='flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded'>
@@ -11,7 +17,13 @@ export const Navbar = () => {
 
 			<div className='flex flex-1'></div>
 
-			<Link className='mr-2' href={'/catalog'}>Catalog</Link>
+			{
+				navItems.map(( item => {
+					return  (
+						<Link className='mr-6' href={item.path}>{item.text}</Link>
+					);
+				}))
+			}
 		</nav>
 	)
 }
