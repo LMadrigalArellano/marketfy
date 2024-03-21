fb519cfa81bd5ae5430e8252be0e1143c4a7016e
LMadrigalArellano@Deloitte.com <LMadrigalArellano@Deloitte.com>
2024-03-21 10:37:40 -0600

    LDMA:
    Created Products Catalog Page
    Created Products Catalog Layout
    
    Updated Home Page

---
+++ app/catalog/page.tsx
@@ -0,0 +1,19 @@
+import { Metadata } from "next";
+import React from "react";
+
+
+export const metadata: Metadata = {
+  title: 'Catalog',
+  description: 'Catalog Page',
+  keywords: ['Catalog page', 'Browse products', 'catalog']
+};
+
+const CatalogPage = () => {
+  return (
+    <>
+      <h1 className="text-7xl">Catalog page</h1>
+    </>
+  )
+}
+
+export default CatalogPage;
