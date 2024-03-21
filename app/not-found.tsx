f45b695992ef475983b107c4f835bc54a80639ed
LMadrigalArellano@Deloitte.com <LMadrigalArellano@Deloitte.com>
2024-03-21 17:01:18 -0600

    LDMA:
    Solved issue with nested anchor tags on not-found pages

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,18 +7,18 @@ export default function NotFound() {
       <div className="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
         Page not found
       </div>
-      <button className="mt-5">
-          <a
+      <Link href="/" className="mt-5">
+          <span
             className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring"
           >
             <span
               className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"
             ></span>
             <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
-              <Link href="/">Return Home</Link>
+              Return Home
             </span>
-          </a>
-        </button>
+          </span>
+        </Link>
     </main>
   )
 }
