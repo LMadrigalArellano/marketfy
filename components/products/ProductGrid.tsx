b87663b46925c167815c45d5083d801a312e34bc
LMadrigalArellano@Deloitte.com <LMadrigalArellano@Deloitte.com>
2024-03-25 13:11:28 -0600

    LDMA:
    Implemented SearchBar for Product Catalog Page

--- components/products/ProductGrid.tsx
+++ components/products/ProductGrid.tsx
@@ -3,6 +3,7 @@
 import { SingleProduct } from "@/interfaces/products/single-product";
 import { ProductCard } from "./ProductCard";
 import { useEffect, useState } from "react";
+import { SearchBar } from "..";
 
 interface Props {
   products: SingleProduct[];
@@ -10,6 +11,7 @@ interface Props {
 
 export const ProductGrid = ({products}: Props) => {
   const [loaded, setLoaded] = useState(false);
+  const [searchText, setSearchText] = useState('');
 
   useEffect(() => {
     setLoaded(true);
@@ -18,15 +20,20 @@ export const ProductGrid = ({products}: Props) => {
   if(!loaded) return <p>Loading...</p>
 
   return (
-    <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 m-10 gap-10'>
-      {
-        products.map((product) => (
-            <ProductCard 
-              key={product.id} 
-              product={ product }
-            />
-        ))
-      }
-    </div>
+    <>
+      <SearchBar setSearchText={setSearchText}/>
+      <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 m-10 gap-10'>
+        {
+          products
+          .filter((product) => product.title.toLowerCase().includes(searchText.toLowerCase()))
+          .map((product) => (
+              <ProductCard 
+                key={product.id} 
+                product={ product }
+              />
+          ))
+        }
+      </div>
+    </>
   )
 }
