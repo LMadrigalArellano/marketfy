e446f5e3b1425646ad736a510128bfea1b102eaf
LMadrigalArellano@Deloitte.com <LMadrigalArellano@Deloitte.com>
2024-03-25 12:53:16 -0600

    LDMA:
    Product Modal implemented

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,8 +1,13 @@
+'use client';
+
 import Link from "next/link";
 import Image from "next/image";
 import { SingleProduct } from "@/interfaces/products/single-product";
 import { AddToCartButton, ToggleFromWishListButton } from "..";
 import { usePathname } from 'next/navigation';
+import { ProductModal } from "./ProductModal";
+import { useState } from "react";
+import { IoClose } from "react-icons/io5";
 
 interface Props { 
   product: SingleProduct
@@ -10,10 +15,13 @@ interface Props {
 
 export const ProductCard = ({ product }: Props) => {
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const pathName = usePathname();
   const { id, title, image, price } = product;
 
   return (
+    <>
       <div className="rounded-md fade-inm-10 w-80">
         <div className="relative">
           <Image 
@@ -23,6 +31,7 @@ export const ProductCard = ({ product }: Props) => {
             width={300}
             height={300}
             priority={false}
+            onClick={() => setIsModalOpen(true)}
           />
           <div className="absolute bottom-0 right-0 p-1">
           <ToggleFromWishListButton product={product}/>
@@ -44,5 +53,25 @@ export const ProductCard = ({ product }: Props) => {
           }
         </div>
       </div>    
+
+      {
+      isModalOpen 
+      && 
+      (
+        <>
+          <div 
+            className="fixed top-0 left-0 w-screen h-screen z-10 bg-black opacity-30"
+          />
+          <div
+            onClick={ () => setIsModalOpen( false )}
+            className="fade-in fixed top-0 left-0 w-screen h-screen z-10 backdrop-filter backdrop-blur-sm"
+          />
+          <ProductModal product={product} closeModal={() => setIsModalOpen(false)} />
+        </>
+      )
+      
+      
+      }
+    </>
   )
 }
--- components/products/ProductModal.tsx
+++ components/products/ProductModal.tsx
@@ -1,16 +1,27 @@
 import { SingleProduct } from "@/interfaces/products/single-product";
 import Image from "next/image";
+import Link from "next/link";
+import { IoCart, IoClose, IoHeartOutline } from "react-icons/io5";
+import { MdAddShoppingCart } from "react-icons/md";
 
 interface Props {
-  product: SingleProduct
+  product: SingleProduct,
+  closeModal: () => void;
 }
 
-export const ProductModal = async ({ product }: Props) => {
+export const ProductModal = ({ product, closeModal }: Props) => {
 
   return (
-    <div className="flex mt-5 flex-col items-center text-slate-800">
-      <div className="relative flex flex-col items-center rounded-[20px] w-[700px] mx-auto bg-white bg-clip-border  shadow-lg  p-3">
+    <div className="flex mt-5 flex-col items-center text-slate-800 absolute justify-center w-screen">
+      <div className="relative flex flex-col items-center rounded-[20px] w-[700px] mx-auto bg-white bg-clip-border shadow-lg  p-3 z-10">
         <div className="mt-2 mb-8 w-full">
+          <div className="flex justify-end mr-3">
+            <IoClose 
+              className="hover:cursor-pointer"
+              size={30} 
+              onClick={closeModal}
+            />
+          </div>
           <h1 className="px-2 text-xl font-bold text-slate-700 capitalize">
             #{product.id} {product.title}
           </h1>
@@ -54,11 +65,14 @@ export const ProductModal = async ({ product }: Props) => {
           </div>
 
           <div className="flex flex-col justify-center rounded-2xl bg-white bg-clip-border px-3 py-4  drop-shadow-lg">
-            <p className="text-sm text-gray-600">
-              Actions
-            </p>
-            <span className="text-base font-medium text-navy-700 flex">
-              Add to cart | Add to wishlist
+            <span className="text-base font-medium text-navy-700 flex hover:text-blue-600 hover:cursor-pointer">
+
+              <Link 
+                className="text-white text-center bg-blue-700 hover:bg-blue-800 focus:ring-4 rounded-lg w-full px-5 py-2.5"
+                href={`catalog/product/${ product.id }`}
+              >
+              View full details
+              </Link>
             </span>
           </div>
         </div>
